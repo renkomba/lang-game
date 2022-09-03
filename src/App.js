274ac642bae190f4cc5b8bd921c6f8b583b5de17
renkomba <61875726+renkomba@users.noreply.github.com>
2022-09-03 00:34:05 -0400

    have App.js handle the canvas reference and canvas context as state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Grid from './components/Grid/Grid';
+import { useEffect, useRef, useState } from 'react';
+import CanvasContext from './contexts/CanvasContext';
 
 function App() {
+  const [ctx, setCtx] = useState(null);
+  // const CanvasContext = useContext(CanvasContext);
+  const canvasRef = useRef(null);
+
+  useEffect(
+    () => {
+      setCtx(canvasRef.current.getContext('2d'));
+      // canvasRef.current.height = 198;
+      // canvasRef.current.width = 352;
+      console.log(canvasRef);
+    },
+    [setCtx]
+  );
+
+  const [ width, height ] = [ 352, 198 ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <CanvasContext.Provider value={ctx}>
+        <div>
+          <canvas
+            ref={canvasRef}
+            height={height}
+            width={width}
+          />
+          <Grid 
+            height={height}
+            width={width}
+            tileSize={64}
+          />
+        </div>
+      </CanvasContext.Provider>
+    </main>
   );
 }
 
