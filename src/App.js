11087e1f8f43bd6b7bd83ee6c59342ef0bc976f1
renkomba <61875726+renkomba@users.noreply.github.com>
2022-09-12 23:38:40 -0400

    ISSUE: too many rerenders. Else, added conditional to useEffect hook to draw sprites to grid

--- src/App.js
+++ src/App.js
@@ -1,48 +1,69 @@
 // import logo from './logo.svg';
+// import CanvasContext from './contexts/CanvasContext';
+// import Canvas from './components/Canvas/Canvas';
+// import Character from './components/Character/Character';
+// import { act, face } from './functions/characterActions';
+// import Grid from './components/Grid/Grid';
+// import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import GameUI from './components/GameUI/GameUI';
+import GameLoop from './components/GameLoop/GameLoop';
+import TileView from './components/TileView/TileView';
 import './App.css';
-import Grid from './components/Grid/Grid';
-import { useEffect, useRef, useState } from 'react';
-import CanvasContext from './contexts/CanvasContext';
-import Canvas from './components/Canvas/Canvas';
+import Character from './character';
+import { createContext } from 'react';
 
+const hero = new Character({ role: 'main' });
+
+
+// Too many rerenders
 function App() {
   const [ctx, setCtx] = useState(null);
   const canvasRef = useRef(null);
+  const PlayerContext = createContext(
+    new Character({ role: 'main' })
+  );
 
   useEffect(
     () => {
       setCtx(canvasRef.current.getContext('2d'));
       // canvasRef.current.height = 198;
       // canvasRef.current.width = 352;
+
+      if (ctx && hero) {
+        let [ startX, startY ] = hero.cutFrom;
+        let [ sizeX, sizeY ] = hero.cutSize;
+        let [ locationX, locationY ] = hero.coordinates;
+        ctx.drawImage(
+          hero.src,
+          startX, startY,
+          sizeX, sizeY,
+          locationX, locationY,
+          hero.spriteSize, hero.spriteSize
+        );
+      }
       console.log(canvasRef);
     },
     [setCtx]
   );
 
-  const draw = (ctx, src='') => {
-    let image = new Image();
-    image.src = src;
-    image.onload = () => ctx.drawImage(image, 0, 0);
-  }
-  const [ width, height ] = [ 352, 198 ];
+  // const printKey = (e) => {
+  //   console.log(e);
+  // }
+  console.log('APP.JS IS RENDERING');
 
   return (
-    <main className="App">
-      <CanvasContext.Provider value={ctx}>
-        <div>
-          <Canvas draw={draw}
-            canvasRef={canvasRef}
-            height={height}
-            width={width}
-          />
-          <Grid 
-            height={height}
-            width={width}
-            tileSize={64}
-          />
-        </div>
-      </CanvasContext.Provider>
-    </main>
+    <>
+      <header>GAME</header>
+      <main className='game_content'>
+        <img
+          id='main_character'
+          className='main_character'
+          src={hero.src}
+        />
+      </main>
+      <footer></footer>
+    </>
   );
 }
 
